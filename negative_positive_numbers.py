63333222bccb986d4b5fbf1c3a2b2c49142c06a4
Sudarshan Mandal <sudarshananjanamandal@gmail.com>
2020-07-01 11:45:51 +0530

    Add files via upload

---
+++ negative_positive_numbers.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the number: "))
+if (a<0):
+    print("{} is a negative number".format(a))
+elif (a==0):
+    print("Entered number is zero")
+else:
+    print("{} is a positive number".format(a))
